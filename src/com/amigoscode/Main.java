e6cff78833a0154d3c0320ea71d8de955e5bafaf
Kienpro1 <trantrungkienl2703@gmai.com>
2023-11-24 11:37:02 +0700

    exercise

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,24 +1,13 @@
     package com.amigoscode;
 
     public class Main {
+    private static double calcYield(double monthlyRent,double Original){
+        return ((monthlyRent*12)/Original)*100;
+    }
+
+
+
     public static void main(String[] args){
-        int a =5;
-        int b=10;
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a++);
-        System.out.println(++a);
-        System.out.println(a > b);
-        System.out.println(a < b);
-        System.out.println(a == b);
-        System.out.println(a != b);
-        System.out.println(a += b);
-        System.out.println(a -= b);
-
-
-
-
+        System.out.println("Rental yield:"+calcYield(1000,100000)+"%");
     }
     }
