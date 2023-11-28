14a80f04af0c712aaf2030cec794d98210aa774a
Kienpro1 <trantrungkienl2703@gmai.com>
2023-11-28 07:59:44 +0700

    update

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,7 +1,73 @@
 package com.amigoscode;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        //local date time
+//        LocalDateTime localDateTime = LocalDateTime.now();
+//        System.out.println(localDateTime);
+//        System.out.println(localDateTime.getDayOfYear());
+//        LocalDateTime someDate = LocalDateTime.of(
+//                2003,
+//                10,
+//                27,
+//                10,
+//                30,
+//                30,
+//                0 /** */
+//        );
+//        System.out.println(someDate);
+        //scanner
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("hãy nhập tên vào :");
+//        String name = scanner.nextLine();
+//        System.out.print("hãy nhập tuổi: ");
+//        int age = scanner.nextInt();
+//        System.out.println("Tên: " + name);
+//        System.out.println("Tuổi: " + age);
+        //try catch
+//        try {
+//            for (int i = 10; i >= 0; i--) {
+//                System.out.println(10 / i);
+//                int a = Integer.parseInt("1x");
+//                System.out.println(a);
+//            }
+//        } catch (Exception e) {
+//            System.out.println(" Error :"+e.getMessage());
+//        }
+        //Working with file
+//           File file =createFile("src/foo.txt");
+//           try
+//           {
+//               FileWriter fileWriter = new FileWriter(file,true);//input file need to write
+//               PrintWriter printWriter = new PrintWriter(fileWriter);//write this file
+//               printWriter.println("Hello World  ");//write in here
+//               printWriter.flush();
+//               printWriter.close();//end write
+//           }catch(IOException e){
+//               System.out.println(e.getMessage());
+//           }
+//
+//    }
+//    private static File createFile (String path){
+//        try {
+//            File file = new File("src/foo.txt");
+//            if(!file.exists()) {
+//                file.createNewFile();
+//            }
+//            return file;
+//        }catch(IOException e) {
+//            System.out.println(e.getMessage());
+//            throw new IllegalStateException(e);
+//        }
+//    }
+
+
+
+
 }
